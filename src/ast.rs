b36e6349a4ec52075115d3eac67b379885b02b91
qylWheels <command1748165360@126.com>
2024-07-16 13:37:30 +0000

    Implement AST

---
+++ src/ast.rs
@@ -0,0 +1,61 @@
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "pest/syntax.pest"]
+pub struct RainParser;
+
+#[derive(Clone, Copy, Debug)]
+pub enum BinOp {
+    Plus,
+    Times,
+    Le,
+    Eq,
+}
+
+#[derive(Clone, Debug)]
+pub enum Expr {
+    Int(i64),
+    Bool(bool),
+    Var(String),
+    Let {
+        x: String,
+        e1: Box<Expr>,
+        e2: Box<Expr>,
+    },
+    If {
+        guard: Box<Expr>,
+        e1: Box<Expr>,
+        e2: Box<Expr>,
+    },
+    Bin {
+        op: BinOp,
+        e1: Box<Expr>,
+        e2: Box<Expr>,
+    },
+}
+
+impl Expr {
+    pub fn is_value(&self) -> bool {
+        match self {
+            Expr::Int(_) | Expr::Bool(_) => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Expr;
+
+    #[test]
+    fn test_is_value() {
+        let e = Expr::Int(4);
+        assert_eq!(e.is_value(), true);
+
+        let e = Expr::Bool(true);
+        assert_eq!(e.is_value(), true);
+
+        let e = Expr::Var("x".into());
+        assert_eq!(e.is_value(), false);
+    }
+}
